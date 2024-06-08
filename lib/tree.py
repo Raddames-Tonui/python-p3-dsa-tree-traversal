be1161f70db104cd17626746c7550cf645f7ac82
Raddames <raddamestonui48@gmail.com>
2024-06-09 00:29:50 +0300

    DEPTH SEARCH

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,33 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  # ------------------------ USING DEPTH --------------------------
+    # def get_element_by_id(self, id):
+    #     if not self.root:
+    #         return None  # Return None if the tree is empty
+
+    #     nodes_to_visit = [self.root]
+
+    #     while nodes_to_visit:
+    #         current_node = nodes_to_visit.pop(0)  # Pop the first element (LIFO for DFS when combined with prepend operation)
+    #         if current_node['id'] == id:
+    #             return current_node  # Return the node if its id matches the given id
+    #         nodes_to_visit = current_node.get('children', []) + nodes_to_visit  # Prepend children to the front of the list
+        
+    #     return None  # Return None if no node with the given id is found
+    
+
+    # # ----------------USING BREADTH ---------------------------
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+        nodes_to_visit = [self.root]
+
+        while nodes_to_visit:
+            current_node = nodes_to_visit.pop(0)
+            if current_node['id'] ==id:
+                return current_node
+            nodes_to_visit = nodes_to_visit + current_node.get('children', [])
+
+        return None
